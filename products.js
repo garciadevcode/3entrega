a980a4da494cf233caf72f4141ccc35b2afeebfd
Andres Garcia <119061295+garciadevcode@users.noreply.github.com>
2023-10-26 20:49:07 -0500

    Update products.js
    Actualizacion

--- products.js
+++ products.js
@@ -1,20 +1,29 @@
 [
     {
         "id": 1,
-        "title": "Producto 3",
-        "price": 1,
-        "stock": 1
+        "title": "Producto 1",
+        "description": "Una silla usada",
+        "price": 32,
+        "thumbnail": "product1.jpg",
+        "code": "P001",
+        "stock": 10
     },
     {
         "id": 2,
-        "title": "Producto 3",
-        "price": 9,
-        "stock": 4
+        "title": "Producto 2",
+        "description": "Una linda mesa de cristal",
+        "price": 40,
+        "thumbnail": "product2.jpg",
+        "code": "P002",
+        "stock": 15
     },
     {
         "id": 3,
         "title": "Producto 3",
-        "price": 15,
-        "stock": 18
+        "description": "Un tetero",
+        "price": 10,
+        "thumbnail": "miproduct3.jpg",
+        "code": "P003",
+        "stock": 8
     }
 ]
